d46601469b80c13c3d0a67098815ece71f02746d
SANTHOSH KUMAR GOWDA <gowda.santhu1939@gmail.com>
2025-04-01 20:03:21 -0400

    Add files via upload

---
+++ python strings.py
@@ -0,0 +1,53 @@
+# practice files:
+# # string=("hello world")
+# # print(string.upper())
+
+# # string=("HI KAVYA")
+# # print(string.lower())
+
+# # string= ("be strong")
+# # print(string.capitalize())
+
+# # string=("    you and me   ")
+# # print(string.strip())
+
+# # str=("you are my knowledge")
+# # print(str.split())
+
+# filename="you are my knowledge"
+# ends_with=filename.endswith("you")
+# print(ends_with)
+
+###########################################################################################
+
+# You are given a string sentence . Print the characters at even indices.
+# sentence = "Python is amazing"
+# print(sentence[::2])
+# # Output: "Pto saaig"
+
+
+# You are given a string s . Replace all spaces in the string with underscores ( _ )and print the modified string.
+# s = "Python is fun and powerful"
+# print(s.replace(" ","_"))
+# # Output: "Python_is_fun_and_powerful"
+
+# Problem:
+# You are given a string s . Check if the string contains only digits.
+
+# s = "12345"
+# print(s.isdigit())
+
+# You are given a string s . Print the string in reverse order.
+
+# s = "Python is amazing"
+# print(s[::-1])
+# # Output: "gnizama si nohtyP
+
+
+# You are given a string s . Capitalize the first letter of each word in the stringand print the modified string.
+
+# s = "python programming is fun"
+# print(s.title())
+# Output: "Python Programming Is Fun"
+
+
